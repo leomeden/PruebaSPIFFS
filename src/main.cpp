7d42e8a15a57108423f4a81a903efa38a524b9d0
leomeden <leonardomeden@gmail.com>
2023-01-13 18:42:37 -0300

    archivo html en flash

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+#include <LittleFS.h>
+
+const char *filename = "index.html";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(5000);
+
+  LittleFS.begin();
+
+  File file = LittleFS.open(filename, "r");
+
+  if (!file) {
+    Serial.println("could not open file for reading");
+  } else {
+
+    while (file.available()) {
+      char buf[32];
+
+      size_t bytesRead = file.readBytes(buf, sizeof(buf) - 1);
+      buf[bytesRead] = '\0';
+
+      Serial.print(buf);
+
+      yield();
+    }
+
+    file.close();
+  }
+
+  LittleFS.end();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
